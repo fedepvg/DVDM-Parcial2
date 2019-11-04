ef50f7f99eeb82a1803b57082a8f50dfb549433f
fedepvg <federicopvg@gmail.com>
2019-11-04 09:51:08 -0300

    mouse rotation added

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    float UpMaxXRotation = 270f;
+    float DownMaxXRotation = 90f;
+    float XAxisRotation;
+
+    private void Start()
+    {
+        PlayerController.OnRotateAction += Rotate;
+    }
+
+    void Rotate(float mouseX, float mouseY)
+    {
+        XAxisRotation += mouseY;
+
+        if (XAxisRotation < -90f)
+        {
+            XAxisRotation = -90f;
+            mouseY = 0f;
+            ClampXRotation(UpMaxXRotation);
+        }
+        else if (XAxisRotation > 90f)
+        {
+            XAxisRotation = 90f;
+            mouseY = 0f;
+            ClampXRotation(DownMaxXRotation);
+        }
+
+        transform.Rotate(Vector3.right * mouseY);
+    }
+
+    void ClampXRotation(float newXRot)
+    {
+        Vector3 eulerRot = transform.eulerAngles;
+        eulerRot.x = newXRot;
+        transform.eulerAngles = eulerRot;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public delegate void OnRotate(float mouseX, float mouseY);
+    public static OnRotate OnRotateAction;
+    public float MouseSensitivity;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        OnRotateAction += Rotate;
+    }
+
+    private void Update()
+    {
+        float horizontal = Input.GetAxis("Mouse X") * MouseSensitivity* Time.deltaTime;
+        float vertical = -Input.GetAxis("Mouse Y") * MouseSensitivity * Time.deltaTime;
+        if(OnRotateAction!=null)
+           OnRotateAction(horizontal, vertical);
+    }
+
+    void Rotate(float mouseX, float mouseY)
+    {
+        transform.Rotate(transform.up * mouseX);
+    }
+}
