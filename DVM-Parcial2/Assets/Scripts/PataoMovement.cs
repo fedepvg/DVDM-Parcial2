7d64bff04868eb5d725b2e4d0577c64e1e6086bf
fedepvg <federicopvg@gmail.com>
2019-11-20 22:07:54 -0300

    level created

---
+++ DVM-Parcial2/Assets/Scripts/PataoMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PataoMovement : MonoBehaviour
+{
+    public Transform PlayerTransform;
+    public float Speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.LookAt(PlayerTransform);
+        transform.localRotation = Quaternion.Euler(0f, transform.rotation.eulerAngles.y, 0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += transform.forward * Speed * Time.deltaTime;
+    }
+}
