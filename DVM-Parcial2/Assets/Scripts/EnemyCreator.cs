b6fdf459323e98b62b161f769634b8655a6d3aaa
fedepvg <federicopvg@gmail.com>
2019-11-22 16:43:11 -0300

    enemy pool added

---
+++ DVM-Parcial2/Assets/Scripts/EnemyCreator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyCreator : MonoBehaviour
+{
+    public GameObject []SpawnPoints;
+    public GameObject []EnemiesPrefabs;
+    public GameObject EnemiesParent;
+
+    const int MaxEnemies = 7;
+    List<GameObject> EnemyList;
+
+    void Start()
+    {
+        EnemyList = new List<GameObject>();
+        for (int i = 0; i < MaxEnemies; i++)
+        {
+            int randomEnemy = Random.Range(0, EnemiesPrefabs.Length);
+            GameObject go = Instantiate(EnemiesPrefabs[randomEnemy], EnemiesParent.transform);
+            go.name = EnemiesPrefabs[randomEnemy].name + i;
+            go.SetActive(false);
+            EnemyList.Add(go);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
